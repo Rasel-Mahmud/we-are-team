8e2e290d6ff8ebd20bfd01caf274826ed68fe58d
Rasel <rasel.wp@gmail.com>
2024-02-18 00:38:39 +0600

    Plugin Refactor

---
+++ readme.txt
@@ -0,0 +1,43 @@
+=== we are team ===
+Contributors: oiiio
+Donate link: https://rashel.pro
+Tags: Team Showcase, Team Members, Employee Directory, Team Slider, Team Introduction
+Requires at least: 4.7
+Tested up to: 6.4
+Stable tag: 1.0.0
+Requires PHP: 7.0
+License: GPLv2 or later
+License URI: https://www.gnu.org/licenses/gpl-2.0.html
+
+We Are Team is a Gutenberg WordPress plugin that provides an easy and elegant way to showcase a group of people on your website.
+
+== Description ==
+
+We Are Team is a Gutenberg WordPress plugin that provides an easy and elegant way to showcase a group of people on your website. Designed with simplicity in mind, this plugin integrates seamlessly with Gutenberg, allowing you to create visually appealing team sections for your blog.
+
+- **Gutenberg Integration:** Effortlessly add and manage team members using the Gutenberg editor.
+- **Visually Appealing Design:** Showcase your team members in a visually appealing and user-friendly way.
+- **Personal Touch:** Add a personal touch to your website by highlighting the people behind your project or organization.
+
+== Frequently Asked Questions ==
+
+= How to install the plugin =
+
+1. Upload the plugin files to the `/wp-content/plugins/werteam` directory.
+2. Activate the plugin through the 'Plugins' menu in WordPress.
+
+= How to use it? =
+
+1. Open a page or post in the Gutenberg editor.
+2. Add a new block and search for "We Are Team."
+3. Start adding team members, including their names, roles, and images.
+
+== Screenshots ==
+
+1. dashboard.png
+2. front-end.png
+
+== Changelog ==
+
+= 1.0 =
+* A change since the previous version.
--- werteam.php
+++ werteam.php
@@ -1,8 +1,8 @@
 <?php
 /*
-* Plugin Name:       We Are Team
-* Plugin URI:        https://rashel.pro
-* Description:       Nice way to show the team member
+* Plugin Name:       We are team
+* Plugin URI:        https://rashel.pro/we-are-team
+* Description:       Introducing 'We Are Team' – a user-friendly Gutenberg plugin designed to effortlessly showcase a group of people on your website.
 * Version:           1.0.0
 * Requires at least: 5.2
 * Requires PHP:      7.2
@@ -11,18 +11,60 @@
 * License:           GPL v2 or later
 * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
 * Update URI:        https://rashel.pro
-* Text Domain:       we-are-team
+* Text Domain:       werteam
 * Domain Path:       /languages
 */
 
-// Setup
+if (!function_exists('add_action')) {
+    echo "Seems like you stumbled here by accident.😛 ";
+    exit;
+}
+
+// Define the plugin path
 define('PLUGIN_PATH', plugin_dir_path(__FILE__));
 
-// Includes
-include(PLUGIN_PATH . 'includes/register-blocks.php');
-include(PLUGIN_PATH . 'includes/theme-settings.php');
+class WeAreTeam {
+    // Define the plugin path
+    private $pluginPath;
 
-// Hooks
-add_action('init', 'up_register_blocks');
-add_action('after_setup_theme', 'we_are_team_image_size');
-add_filter('image_size_names_choose', 'we_are_team_image_size_add');
+    // Constructor
+    public function __construct() {
+        $this->pluginPath = plugin_dir_path(__FILE__);
+
+        // Include necessary files
+        $this->includeFiles();
+
+        // Register hooks
+        add_action('init', [$this, 'registerBlocks']);
+        add_action('after_setup_theme', [$this, 'registerImageSize']);
+        add_filter('image_size_names_choose', [$this, 'addImageSize']);
+    }
+
+    // Include necessary files
+    private function includeFiles() {
+        include($this->pluginPath . 'includes/register-blocks.php');
+        include($this->pluginPath . 'includes/theme-settings.php');
+    }
+
+    // Register blocks
+    public function registerBlocks() {
+        up_register_blocks();
+    }
+
+    // Register image size
+    public function registerImageSize() {
+        we_are_team_image_size();
+    }
+
+    // Add image size to choose list
+    public function addImageSize($sizes) {
+        return we_are_team_image_size_add($sizes);
+    }
+}
+
+// Instantiate the class on 'plugins_loaded'
+function kickOff_weAreTeam_plugin() {
+    $weAreTeamPlugin = new WeAreTeam();
+}
+
+add_action('plugins_loaded', 'kickOff_weAreTeam_plugin');
